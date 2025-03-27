c2e1fcc83b7b88e68fce635ed3846578924e9e90
Astamp9 <andrew_de_stamps@yahoo.com>
2025-03-27 16:02:54 -0400

    app displays server data

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const movies = [
-    {title: 'Mean Girls'},
-    {title: 'Hackers'},
-    {title: 'The Grey'},
-    {title: 'Sunshine'},
-    {title: 'Ex Machina'},
-  ];
+  // const movies = [
+  //   {title: 'Mean Girls'},
+  //   {title: 'Hackers'},
+  //   {title: 'The Grey'},
+  //   {title: 'Sunshine'},
+  //   {title: 'Ex Machina'},
+  // ];
+
+  const [titles, setTitles] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/titles')
+      .then(res => res.json())
+      .then(data => {
+        setTitles(data);
+      })
+    }, [])
 
   return (
     <>
       <div>
         <ul>
-       {movies.map((movie, index) => ( 
-         <li key ={index} > {movie.title} </li> )
+       {titles.map((movie, index) => ( 
+       
+         <li key ={movie.id} > {movie.title} </li> )
         )}
          </ul>
       </div>
